8791ec13b2ef4f9fc1b2a7ce7312142ef18868c3
gutsy47 <bestcom5055@gmail.com>
2022-10-23 01:51:42 +0300

    Feat: Task 2 (1-4). Sort array
    Task 2 (1-4) completed.
    Action '2' (sort the array) has been created. Provided sorts: bubble, shaker, comb and insert
    QuickSort will be done soon
    
    Signed-off-by: gutsy47 <bestcom5055@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,13 +2,79 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
 
-void fillArray(short arr[], int length) {
-    /*
-     * Fills array with pseudo-random nums in range [-99, 99]
-     * :param arr: int [] - array to fill
-     * :param length: int - length of array
-     */
+
+void bubbleSort(int arr[], int length) {
+    for (int i = length - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+
+void shakerSort(int arr[], int length) {
+    for (int i = length - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+        for (int j = i - 1; j > length - i; j--) {
+            if (arr[j] < arr[j - 1]) {
+                std::swap(arr[j], arr[j - 1]);
+            }
+        }
+    }
+}
+
+
+void combSort(int arr[], int length) {
+    int gap = length;
+
+    while (gap != 1) {
+        gap /= 1.247;
+        if (gap < 1) {
+            gap = 1;
+        }
+
+        for (int i = 0; i < length - gap; i++) {
+            if (arr[i] > arr[i + gap]) {
+                std::swap(arr[i], arr[i + gap]);
+            }
+        }
+    }
+}
+
+
+void insertSort(int arr[], int length) {
+    for (int i = 1; i < length; i++) {
+        for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+            std::swap(arr[j - 1], arr[j]);
+        }
+    }
+}
+
+
+void quickSort(int arr[], int length) {
+
+}
+
+
+void printArray(int arr[], int length) {
+    // Just print array
+    for (int i = 0; i < length; i++) {
+        std::cout << arr[i] << ' ';
+    }
+    putchar('\n');
+}
+
+
+void fillArray(int arr[], int length) {
+    // Fill array with pseudo-random nums in range [-99, 99]
     std::srand(time(NULL));
     for (int i = 0; i < length; i++) {
         arr[i] = -99 + (std::rand() % 199);
@@ -22,9 +88,10 @@ int main() {
     std::cout << "Enter 'h' to get list of commands\n";
 
     // Array initialization
-    const short N = 100;
-    short array[N];
+    const int N = 100;
+    int array[N], unsortedArray[N];
     fillArray(array, N);
+    std::copy(std::begin(array), std::end(array), std::begin(unsortedArray));
 
     // Main loop
     while (true) {
@@ -49,12 +116,20 @@ int main() {
                 break;
             }
 
+            // Sort the array
+            case '2': {
+                auto start = std::chrono::steady_clock::now();
+                combSort(array, N);
+                auto end = std::chrono::steady_clock::now();
+                auto elapsed_us = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+                std::cout << "Array sorted. Elapsed time: " << elapsed_us << " ns\n";
+                break;
+            }
+
             // Display array elements
             case '9': {
-                for (short element : array) {
-                    std::cout << element << ' ';
-                }
-                std::cout << std::endl;
+                printArray(array, N);
+                printArray(unsortedArray, N);
                 break;
             }
 
